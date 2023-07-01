68b5659e1b9d91a04ae2e7d625be6ef67839c5b4
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-07-01 14:32:05 +0300

    Update index.ts
    I have added some advanced functionalities
    The Ticket Management Canister now includes the following advanced functionalities:
    
    1. `Ticket Availability Check`: The `checkTicketAvailability(id)` function allows users to check the availability of tickets for a specific event. By providing the event ticket ID, users can determine whether there are any tickets remaining for sale. This functionality helps users make informed decisions before purchasing tickets and avoids overselling.
    
    2. `Ticket Reservations`: The `reserveTicket(id, username)` function enables users to reserve a ticket for a specific event. Users can provide the event ticket ID and their username to reserve a ticket temporarily. This functionality prevents multiple users from reserving the same ticket simultaneously, ensuring fair access to available tickets.
    
    3. `Ticket Transfer`: The `transferTicket(id, newOwner)` function facilitates the transfer of ticket ownership from one user to another. Users can specify the ticket ID and the username of the new owner to initiate the transfer process. This feature allows users to easily transfer tickets they have purchased to others, enabling ticket sharing and gifting.
    
    4. `Ticket Refunds`: The` requestTicketRefund(id)` function enables users to request a refund for a purchased ticket. By providing the ticket ID, users can initiate the refund process, canceling the ticket and initiating the refund. This functionality provides flexibility and convenience for users who may need to cancel their attendance and obtain a refund.

--- src/index.ts
+++ src/index.ts
@@ -122,7 +122,7 @@ export function buyTicket(
 }
 
 $update;
-export function resellTIcket(
+export function resellTicket(
   id: string,
   username: string
 ): Result<TicketSold, string> {
@@ -142,6 +142,83 @@ export function resellTIcket(
   ticketSoldStorage.insert(newTicket.id, newTicket);
 
   return Result.Ok<TicketSold, string>(newTicket);
+}
+
+$query;
+export function checkTicketAvailability(id: string): Result<boolean, string> {
+  const eventTicket = getEventTicketById(id);
+  
+  if (eventTicket.isErr()) {
+    return Result.Err<boolean, string>(
+      `Event ticket with id=${id} not found.`
+    );
+  }
+  
+  const ticket = eventTicket.unwrap();
+  const availableTickets = ticket.totalTicketSold < ticket.capacity;
+  
+  return Result.Ok<boolean, string>(availableTickets);
+}
+
+$update;
+export function reserveTicket(id: string, username: string): Result<string, string> {
+  const eventTicket = getEventTicketById(id);
+  
+  if (eventTicket.isErr()) {
+    return Result.Err<string, string>(
+      `Event ticket with id=${id} not found.`
+    );
+  }
+  
+  const ticket = eventTicket.unwrap();
+  
+  if (ticket.reservedBy && ticket.reservedBy !== username) {
+    return Result.Err<string, string>(`Ticket is already reserved by another user.`);
+  }
+  
+  const updatedTicket = {
+    ...ticket,
+    reservedBy: username,
+  };
+  
+  eventTicketStorage.insert(updatedTicket.id, updatedTicket);
+  
+  return Result.Ok<string, string>(updatedTicket.id);
+}
+
+$update;
+export function transferTicket(id: string, newOwner: string): Result<string, string> {
+  const ticket = getTicketSoldById(id);
+  
+  if (ticket.isErr()) {
+    return Result.Err<string, string>(
+      `Ticket sold with id=${id} not found.`
+    );
+  }
+  
+  const updatedTicket = {
+    ...ticket.unwrap(),
+    username: newOwner,
+  };
+  
+  ticketSoldStorage.insert(updatedTicket.id, updatedTicket);
+  
+  return Result.Ok<string, string>(updatedTicket.id);
+}
+
+$update;
+export function requestTicketRefund(id: string): Result<string, string> {
+  const ticket = getTicketSoldById(id);
+  
+  if (ticket.isErr()) {
+    return Result.Err<string, string>(
+      `Ticket sold with id=${id} not found.`
+    );
+  }
+  
+  ticketSoldStorage.remove(id);
+  
+  return Result.Ok<string, string>(id);
 }
 
 // a workaround to make uuid package work with Azle
