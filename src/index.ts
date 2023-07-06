b8c4f17d6d02568d1b01f7e379624cc762dcca1a
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-07-06 11:44:30 +0300

    Added improvements to the tcket app

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import {
   nat64,
   ic,
   Opt,
+  Principal,
+  nat8,
+  $init,
 } from "azle";
 import { v4 as uuidv4 } from "uuid";
 
@@ -34,9 +37,64 @@ type EventTicketPayload = Record<{
   price: number;
 }>;
 
+var eventAdmin : Principal;
+
 const eventTicketStorage = new StableBTreeMap<string, EventTicket>(0, 44, 1024);
 
 const ticketSoldStorage = new StableBTreeMap<string, TicketSold>(0, 44, 1024);
+
+//
+let organizerCount : nat8 = 0;
+const eventOrganizers = new StableBTreeMap<nat8,Principal>(1,100,1000);
+
+//specify the admin of the contract on deployment
+$init;
+export function init(admin : string) : void{
+  eventAdmin = Principal.fromText(admin);
+}
+
+
+//check of user is Admin or event organizer
+$query;
+export function isOrganizer( id : string) : boolean{
+  const isorganizer = eventOrganizers.values().filter((organizer) => organizer.toString() === id);
+  if(isorganizer.length > 0){
+      return true;
+  }else if(Principal.fromText(id) === eventAdmin){
+      return true;
+  }
+  return false;
+}
+
+
+//add organizer by the admin
+$update;
+export function addOrganizer( org : string) : Result<string, string>{
+  if(org.length !== 0 && isOrganizer(ic.caller().toString())){
+
+    eventOrganizers.insert(organizerCount,Principal.fromText(org));
+    return Result.Ok<string,string>("New organizer added successfully");
+  }
+  return Result.Err<string,string>("Unable to add new Organizer");
+}
+
+
+//delete an organizer by an admin or fellow organizer
+$update;
+export function deleteOrganizer(id : nat8) : Result<string,string>{
+  if(isOrganizer(ic.caller().toString())){
+    return match(eventOrganizers.remove(id),{
+      Some : () =>{ return Result.Ok<string,string>("Organizer deleted successfully")},
+      None : ()=>{ return Result.Err<string,string>("organizer cannot be deleted")}
+    });
+  }
+  return Result.Err<string,string>("You dont have permissions to delete an organizer")
+}
+
+
+
+
+
 
 $query;
 export function getAllEventTickets(): Result<Vec<EventTicket>, string> {
@@ -47,6 +105,13 @@ $update;
 export function createEventTicket(
   payload: EventTicketPayload
 ): Result<EventTicket, string> {
+
+  const caller = ic.caller().toString();
+  if(!isOrganizer(caller)){
+    return Result.Err<EventTicket,string>("Only Event organizers can create tickets");
+  }
+
+
   const newTicket: EventTicket = {
     id: uuidv4(),
     createdAt: ic.time(),
@@ -67,8 +132,14 @@ export function getEventTicketById(id: string): Result<EventTicket, string> {
   });
 }
 
+//delete a ticket by the event organizer
 $update;
 export function deleteEventTicket(id: string): Result<EventTicket, string> {
+  const caller = ic.caller().toString();
+  if(!isOrganizer(caller)){
+    return Result.Err<EventTicket,string>("You are not authorized to delete tickets")
+  }
+
   return match(eventTicketStorage.remove(id), {
     Some: (ticket) => Result.Ok<EventTicket, string>(ticket),
     None: () =>
@@ -78,6 +149,7 @@ export function deleteEventTicket(id: string): Result<EventTicket, string> {
   });
 }
 
+//get a sold ticket by its id
 $query;
 export function getTicketSoldById(id: string): Result<TicketSold, string> {
   return match(ticketSoldStorage.get(id), {
@@ -87,11 +159,18 @@ export function getTicketSoldById(id: string): Result<TicketSold, string> {
   });
 }
 
+//buy a ticket
 $update;
 export function buyTicket(
   id: string,
   username: string
 ): Result<TicketSold, string> {
+
+  const caller = ic.caller().toString();
+
+  if(isOrganizer(caller)){
+    return Result.Err<TicketSold,string>("Event organizers cannot buy their own tickets");
+  }
   const eventTicket = getEventTicketById(id);
 
   if (eventTicket.isErr()) {
@@ -121,6 +200,7 @@ export function buyTicket(
   return Result.Ok<TicketSold, string>(newTicket);
 }
 
+//resell a ticket by its owner
 $update;
 export function resellTIcket(
   id: string,
